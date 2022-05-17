657affc6dab890cb16eb82c3586a456fc416b17c
roberto.torres <ts.roberto@gmail.com>
2022-05-17 13:43:41 +0100

    using sharedcomponents package
    getOwner needs to be updated

--- src/lib/SolWidget.js
+++ src/lib/SolWidget.js
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from "react"
-import { useMachine } from "@xstate/react"
-import { widgetMachine } from "./widgetMachine"
-import { encode } from "base64-arraybuffer"
-import { getProxyAsset } from "./utils"
-import Header from "./Header"
-import Player from "./Player"
-import "./db.css"
-import Panel from "./Panel"
+import React, { useEffect, useState } from "react";
+import {
+  Header,
+  Panel,
+  Player,
+  utils,
+  widgetMachine,
+} from "@darkblock.io/shared-components";
+import { useMachine } from "@xstate/react";
+import { encode } from "base64-arraybuffer";
+import "./db.css";
 
-const platform = "Solana"
-const contractAddress = ""
+const platform = "Solana";
+const contractAddress = "";
 
-let signature, epochSignature
+let signature, epochSignature;
 
 const SolanaDarkblockWidget = ({
   tokenId = "",
@@ -27,41 +29,44 @@ const SolanaDarkblockWidget = ({
     },
   },
 }) => {
-  const [state, send] = useMachine(() => widgetMachine(tokenId, contractAddress, platform))
-  const [mediaURL, setMediaURL] = useState("")
+  const [state, send] = useMachine(() =>
+    widgetMachine(tokenId, contractAddress, platform)
+  );
+  const [mediaURL, setMediaURL] = useState("");
 
   const callback = (state) => {
-    if (config.debug) console.log("Callback function called from widget. State: ", state)
+    if (config.debug)
+      console.log("Callback function called from widget. State: ", state);
 
-    if (typeof cb !== "function") return
+    if (typeof cb !== "function") return;
 
     try {
-      cb(state)
+      cb(state);
     } catch (e) {
-      console.log("Callback function error: ", e)
+      console.log("Callback function error: ", e);
     }
-  }
+  };
 
   const printWalletAdapter = () => {
     if (config.debug) {
-      console.log("walletAdapter: ", walletAdapter)
+      console.log("walletAdapter: ", walletAdapter);
     }
-  }
+  };
 
   useEffect(() => {
-    callback(state.value)
+    callback(state.value);
 
     if (walletAdapter && walletAdapter.connected === false) {
-      printWalletAdapter()
-      send({ type: "DISCONNECT_WALLET" })
+      printWalletAdapter();
+      send({ type: "DISCONNECT_WALLET" });
     }
 
     if (state.value === "idle") {
-      send({ type: "FETCH_ARWEAVE" })
+      send({ type: "FETCH_ARWEAVE" });
     }
 
     if (state.value === "started" && walletAdapter.connected) {
-      send({ type: "CONNECT_WALLET" })
+      send({ type: "CONNECT_WALLET" });
     }
 
     if (state.value === "start_failure") {
@@ -69,20 +74,20 @@ const SolanaDarkblockWidget = ({
     }
 
     if (state.value === "wallet_connected") {
-      printWalletAdapter()
+      printWalletAdapter();
     }
 
     if (state.value === "signing") {
-      authenticate()
+      authenticate();
     }
 
     if (state.value === "authenticated") {
-      send({ type: "DECRYPT" })
+      send({ type: "DECRYPT" });
     }
 
     if (state.value === "decrypting") {
       setMediaURL(
-        getProxyAsset(
+        utils.getProxyAsset(
           state.context.artId,
           epochSignature,
           state.context.tokenId,
@@ -90,53 +95,63 @@ const SolanaDarkblockWidget = ({
           null,
           platform
         )
-      )
+      );
       setTimeout(() => {
-        send({ type: "SUCCESS" })
-      }, 2000)
+        send({ type: "SUCCESS" });
+      }, 2000);
     }
-  }, [state.value, walletAdapter.connected])
+  }, [state.value, walletAdapter.connected]);
 
   const authenticate = async () => {
-    let epoch = Date.now()
-    let address = null
+    let epoch = Date.now();
+    let address = null;
 
     if (walletAdapter.publicKey && walletAdapter.signMessage) {
-      address = walletAdapter.publicKey.toBase58()
-      const message = new TextEncoder().encode(epoch + address)
-      signature = null
+      address = walletAdapter.publicKey.toBase58();
+      const message = new TextEncoder().encode(epoch + address);
+      signature = null;
 
       try {
-        signature = await walletAdapter.signMessage(message, "utf8")
+        signature = await walletAdapter.signMessage(message, "utf8");
       } catch (e) {
-        console.log(e)
+        console.log(e);
       } finally {
         if (signature) {
           if (state.context.owner.owner_address === address) {
-            signature = encodeURIComponent(encode(signature)) + "_Solana"
-            epochSignature = epoch + "_" + signature
-            send({ type: "SUCCESS" })
+            signature = encodeURIComponent(encode(signature)) + "_Solana";
+            epochSignature = epoch + "_" + signature;
+            send({ type: "SUCCESS" });
           } else {
-            send({ type: "FAIL" })
+            send({ type: "FAIL" });
           }
         }
 
-        send({ type: "CANCEL" })
+        send({ type: "CANCEL" });
       }
     }
-  }
+  };
 
   return (
-    <div className={config.customCssClass ? `DarkblockWidget-App ${config.customCssClass}` : `DarkblockWidget-App`}>
+    <div
+      className={
+        config.customCssClass
+          ? `DarkblockWidget-App ${config.customCssClass}`
+          : `DarkblockWidget-App`
+      }
+    >
       {state.value === "display" ? (
-        <Player mediaType={state.context.display.fileFormat} mediaURL={mediaURL} config={config.imgViewer} />
+        <Player
+          mediaType={state.context.display.fileFormat}
+          mediaURL={mediaURL}
+          config={config.imgViewer}
+        />
       ) : (
         <Header state={state} authenticate={() => send({ type: "SIGN" })} />
       )}
       <Panel state={state} />
       {config.debug && <p>{state.value}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default SolanaDarkblockWidget
+export default SolanaDarkblockWidget;
